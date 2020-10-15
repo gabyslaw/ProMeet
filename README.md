3aeb8c1d62d1d9074a6b929dba243ff31a70ef66
gabyslaw <gabyslaw@gmail.com>
2020-10-15 11:06:02 +0100

    Updated Chat Fragments

---
+++ README.md
@@ -0,0 +1,11 @@
+# ProMeet
+
+ProMeet is the next big thing
+
+Level Up Your Life
+
+Find an Expert
+
+Not sure how to get your startup funded? Do you need direction for your marketing strategy? Or maybe you want some guidance on how to develop a new product? 
+
+Use ProMeet
---
+++ app/src/main/java/gabytech/promeet/SettingsActivity.java
@@ -0,0 +1,84 @@
+package gabytech.promeet;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
+
+    //Layout
+
+    private CircleImageView mDisplayImage;
+    private TextView mName;
+    private TextView mStatus;
+
+    private Button mStatusBtn;
+    private Button mImageBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mDisplayImage = findViewById(R.id.settings_image);
+        mName = findViewById(R.id.settings_display_name);
+        mStatus = findViewById(R.id.settings_status);
+        mStatusBtn = findViewById(R.id.settings_status_btn);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String current_uid = mCurrentUser.getUid();
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.child("name").getValue().toString();
+               // String image = dataSnapshot.child("image").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+               // String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+                mStatus.setText(status);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String status_value = mStatus.getText().toString();
+
+                Intent status_intent = new Intent(SettingsActivity.this, StatusActivity.class);
+                status_intent.putExtra("status_value", status_value);
+                startActivity(status_intent);
+            }
+        });
+    }
+}
